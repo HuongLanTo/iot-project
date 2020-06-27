ecfdd8aa08935b984313b4f996b8f215a784581a
BossTung <tung261298@gmail.com>
2020-06-27 22:40:11 +0700

    update createNode 20200627

--- server/api/mongo/node/createNode.js
+++ server/api/mongo/node/createNode.js
@@ -22,15 +22,97 @@ const createNode = async function createNode(req, res) {
     created_by: body.created_by,
     location: body.location,
     ip: body.ip,
-    name: body.name,
-    temperature_status: body.temperature_status === 1 ? 1 : 0,
-    humidity_status: body.humidity_status === 1 ? 1 : 0,
-    dust_status: body.dust_status === 1 ? 1 : 0,
-    co_status: body.co_status === 1 ? 1 : 0,
-    nh3_status: body.nh3_status === 1 ? 1 : 0,
-    co2_status: body.co2_status === 1 ? 1 : 0,
-    smoke_status: body.smoke_status === 1 ? 1 : 0,
+    name: body.name
   });
+
+  if (body.temperature_status != null) {
+    if (body.temperature_status == 1 || body.temperature_status == 0) {
+      value.temperature_status = body.temperature_status;
+    } else {
+      logger.info("Param invalid");
+      return res.status(400).send({
+        responseCode: 11,
+        responseMessage: "Trạng thái NHIỆT ĐỘ không hợp lệ",
+      });
+    }
+  }
+  if (body.humidity_status != null) {
+    if (body.humidity_status == 1 || body.humidity_status == 0) {
+      value.humidity_status = body.humidity_status;
+    } else {
+      logger.info("Param invalid");
+      return res.status(400).send({
+        responseCode: 11,
+        responseMessage: "Trạng thái ĐỘ ẨM không hợp lệ",
+      });
+    }
+  }
+  if (body.dust_status != null) {
+    if (body.dust_status == 1 || body.dust_status == 0) {
+      value.dust_status = body.dust_status;
+    } else {
+      logger.info("Param invalid");
+      return res.status(400).send({
+        responseCode: 11,
+        responseMessage: "Trạng thái BỤI không hợp lệ",
+      });
+    }
+  }
+  if (body.co_status != null) {
+    if (body.co_status == 1 || body.co_status == 0) {
+      value.co_status = body.co_status;
+    } else {
+      logger.info("Param invalid");
+      return res.status(400).send({
+        responseCode: 11,
+        responseMessage: "Trạng thái CO không hợp lệ",
+      });
+    }
+  }
+  if (body.pressure_status != null) {
+    if (body.pressure_status == 1 || body.pressure_status == 0) {
+      value.pressure_status = body.pressure_status;
+    } else {
+      logger.info("Param invalid");
+      return res.status(400).send({
+        responseCode: 11,
+        responseMessage: "Trạng thái ÁP SUẤT không hợp lệ",
+      });
+    }
+  }
+  if (body.nh3_status != null) {
+    if (body.nh3_status == 1 || body.nh3_status == 0) {
+      value.nh3_status = body.nh3_status;
+    } else {
+      logger.info("Param invalid");
+      return res.status(400).send({
+        responseCode: 11,
+        responseMessage: "Trạng thái NH3 không hợp lệ",
+      });
+    }
+  }
+  if (body.co2_status != null) {
+    if (body.co2_status == 1 || body.co2_status == 0) {
+      value.co2_status = body.co2_status;
+    } else {
+      logger.info("Param invalid");
+      return res.status(400).send({
+        responseCode: 11,
+        responseMessage: "Trạng thái CO2 không hợp lệ",
+      });
+    }
+  }
+  if (body.smoke_status != null) {
+    if (body.smoke_status == 1 || body.smoke_status == 0) {
+      value.smoke_status = body.smoke_status;
+    } else {
+      logger.info("Param invalid");
+      return res.status(400).send({
+        responseCode: 11,
+        responseMessage: "Trạng thái KHÓI không hợp lệ",
+      });
+    }
+  }
 
   try {
     const saveNode = await value.save();
