e5040518582be939f11f791774ed05145638893c
Huong Lan To <tohuonglan1705@gmail.com>
2020-06-29 12:33:22 +0700

    add the validation for inputs in the node creation function

--- web_client/src/app/pages/new-node/new-node.component.ts
+++ web_client/src/app/pages/new-node/new-node.component.ts
@@ -1,56 +1,134 @@
-import { Component, OnInit } from '@angular/core';
-import { District, district } from 'src/app/data/district';
-import { Http } from '@angular/http';
-import { NodeService } from 'src/app/services/node.service';
-import { ToastrService } from 'ngx-toastr';
-import { Router } from '@angular/router';
-
+import { Component, OnInit } from "@angular/core";
+import { District, district } from "src/app/data/district";
+import { Http } from "@angular/http";
+import { NodeService } from "src/app/services/node.service";
+import { ToastrService } from "ngx-toastr";
+import { Router } from "@angular/router";
+import { LocationService } from "src/app/services/location.service";
 
 @Component({
-  selector: 'app-new-node',
-  templateUrl: './new-node.component.html',
-  styleUrls: ['./new-node.component.css']
+  selector: "app-new-node",
+  templateUrl: "./new-node.component.html",
+  styleUrls: ["./new-node.component.css"],
 })
 export class NewNodeComponent implements OnInit {
   public newNode = {
-    id: '',
-    nameNode: '',
-    location: '',
-    address: '',
-    so2: '',
-    no: '',
-    buipm25: '',
-    temperature: '',
-    humidity: '',
-    latitude: '',
-    longitude: ''
+    id: "",
+    nameNode: "",
+    location: "",
+    address: "",
+    so2: "",
+    no: "",
+    buipm25: "",
+    temperature: "",
+    humidity: "",
+    latitude: "",
+    longitude: "",
   };
-  district: District[] = district;
-  
+  public location: any;
+
+  // declare invalid variables
+  isNodenameInvalid: boolean = true;
+  isLocationInvalid: boolean = false;
+  isLatitudeInvalid: boolean = false;
+  isLongitudeInvalid: boolean = false;
+  isAddressInvalid: boolean = false;
+
+
   constructor(
     private http: Http,
     private nodeService: NodeService,
     private toastrService: ToastrService,
-    private router: Router
-  ) { }
+    private router: Router,
+    private locationService: LocationService
+  ) {}
 
-  ngOnInit() {
-    console.log(this.newNode);
-
+  async ngOnInit() {
+    await this.locationService.getLocationList().then((data) => {
+      this.location = data;
+    });
   }
 
-  saveNode() {
-    this.nodeService.createNode(this.newNode).then(data => {
-      this.toastrService.success('Tạo mới node thành công');
-      this.router.navigate(['/node']);
-    }).catch(err => {
-      this.toastrService.warning('Tạo mới node thất bại');
-    })
+  createNode() {
+    if (!this.newNode.location) {
+      this.isLocationInvalid = true;
+    } else {
+      this.isLocationInvalid = false;
+    }
+    if (!this.newNode.latitude) {
+      this.isLatitudeInvalid = true;
+    } else {
+      this.isLatitudeInvalid = false;
+    }
+    if (!this.newNode.longitude) {
+      this.isLongitudeInvalid = true;
+    } else {
+      this.isLongitudeInvalid = false;
+    }
+    if (!this.newNode.address) {
+      this.isAddressInvalid = true;
+    } else {
+      this.isAddressInvalid = false;
+    }
+    if (!this.isLatitudeInvalid && !this.isLongitudeInvalid && !this.isLocationInvalid) {
+      this.nodeService
+        .createNode(this.newNode)
+        .then((data) => {
+          this.toastrService.success("Tạo mới node thành công");
+          this.router.navigate(["/node"]);
+        })
+        .catch((err) => {
+          this.toastrService.warning("Tạo mới node thất bại");
+        });
+    }
   }
 
   redirect() {
-    this.router.navigate(['/node']);
+    this.router.navigate(["/node"]);
   }
 
+  checkLatitude(value: any) {
+    if (value) {
+      this.isLatitudeInvalid = false;
+    } else {
+      this.isLatitudeInvalid = true;
+    }
+  }
 
+  checkLongitude(value: any) {
+    if (value) {
+      this.isLongitudeInvalid = false;
+    } else {
+      this.isLongitudeInvalid = true;
+    }
+  }
+
+  checkAddress(value: any) {
+    if (value) {
+      this.isAddressInvalid = false;
+    } else {
+      this.isAddressInvalid = true;
+    }
+  }
+
+  checkLocation(value: any) {
+    if (value) {
+      this.isLocationInvalid = false;
+      this.newNode.location = value;
+      var numberNode = this.getNumberOfNode(value) + 1;
+      this.newNode.nameNode = value + '_' + numberNode;
+      this.isNodenameInvalid = false;
+    } else {
+      this.isLocationInvalid = true;
+    }
+  }
+
+  getNumberOfNode(value: any):number {
+    for (var i = 0; i < this.location.length; i++) {
+      if (this.location[i].name === value) {
+        return this.location[i].numberNode;
+      }
+    }
+    
+  }
 }
---
+++ web_client/src/app/services/location.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class LocationService {
+    constructor(
+        private http: Http
+    ) {}
+
+    private API_URL = "https://5ee4a4deddcea00016a36e04.mockapi.io/api";
+
+    getLocationList() {
+        return new Promise((resolve, reject) => {
+            this.http.get(this.API_URL + '/location').subscribe(
+                res => {
+                    var object = JSON.parse((<any>res)._body);
+                    resolve(object)
+                }, err => {
+                    reject(err)
+                }
+            )
+        })
+    }
+}
