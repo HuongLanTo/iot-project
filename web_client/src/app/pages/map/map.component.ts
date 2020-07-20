4e09187e17fb0a2faf47ad34b77292d0693d123b
Huong Lan To <tohuonglan1705@gmail.com>
2020-07-20 22:18:19 +0700

    update map

--- web_client/src/app/pages/map/map.component.ts
+++ web_client/src/app/pages/map/map.component.ts
@@ -6,6 +6,7 @@ import { OpenStreetMapProvider } from 'leaflet-geosearch';
 // add to leaflet
 import { GeoSearchControl, SearchControl } from 'leaflet-geosearch';
 import { SensorData, sensorData } from 'src/app/data/sensor_data';
+import { DataService } from 'src/app/services/data.service';
 const iconRetinaUrl = 'assets/marker-icon-2x.png';
 const iconUrl = 'assets/marker-icon.png';
 const shadowUrl = 'assets/marker-shadow.png';
@@ -41,18 +42,56 @@ interface result {
 })
 export class MapComponent implements OnInit {
   private map;
-  sensor_data: SensorData[] = sensorData;
-  constructor() { }
+  sensor_data: any;
+  polygon = [];
 
-  ngOnInit(): void {
+  constructor(
+    private dataService: DataService
+  ) { }
+
+  async ngOnInit() {
     this.initMap();
-    var marker = L.marker([20.975986, 105.815191]).addTo(this.map);
-    marker.bindPopup("<b>nameNode1</b><br>AQI:96");
+    // var marker = L.marker([20.975986, 105.815191]).addTo(this.map);
+    // marker.bindPopup("<b>nameNode1</b><br>AQI:96");
+    await this.getDataByLastHour();
+    // this.sensor_data.forEach(e => {
+    //   this.polygon.push([Number(e.long), Number(e.lat)]);
+    // });
+    // console.log(145, this.sensor_data);
+    // console.log();
+    //     console.log(this.polygon);
+        
+    // for (var i = 0; i < this.polygon.length; i++) {
+    //   var markerLocation = new L.LatLng(this.polygon[i][0], this.polygon[i][1]);
+    //   var marker = new L.Marker(markerLocation);
+    //   this.map.addLayer(marker);
+    // }
+    var location = "Thanh Xuan";
+    var markers = [
+      [ -0.1244324, 51.5006728, "Big Ben" ],
+      [ -0.119623, 51.503308, "London Eye" ],
+      [ -0.1279688, 51.507728, `<b>${location}</b><table style="width: 100%;"><tbody><td><i class="fa fa-tint fa-2x" aria-hidden="true" style="float:left;margin-right:6px;"><span style="font-size:18px">30</span></i></td><td><i class="fa fa-tint fa-2x" aria-hidden="true" style="float:left;margin-right:6px;"><span>30</span></i></td></tbody></table>`] 
+   ];
+   
+   //Loop through the markers array
+    for (var i=0; i<markers.length; i++) {
+     
+      var lon = Number(markers[i][0]);
+      var lat = Number(markers[i][1]);
+      var popupText = String(markers[i][2]);
+      
+       var markerLocation = new L.LatLng(lat, lon);
+       var marker = new L.Marker(markerLocation);
+       this.map.addLayer(marker);
+   
+       marker.bindPopup(popupText);
+   
+    }
   }
 
-  private initMap(): void {
+  private initMap() {
     this.map = L.map('map', {
-      center: [20.975986, 105.815191],
+      center: [51.5056,-0.1213],
       zoom: 15
     });
 
@@ -63,5 +102,27 @@ export class MapComponent implements OnInit {
 
     tiles.addTo(this.map);
   }
+
+  async getDataByLastHour() {
+    await this.dataService.getDataByLastHour().then(data => {
+      console.log(data);
+      this.sensor_data = data;
+    });
+    // await this.setMarker();
+  }
+
+  // setMarker() {
+  //   console.log(56465348756453);
+    
+  //   var polygon: any = [];
+  //   this.sensor_data.forEach(e => {
+  //     polygon.push([Number(e.long), Number(e.lat)]);
+  //   });
+  //   for (var i = 0; i < polygon.length; i++) {
+  //     var markerLocation = new L.LatLng(polygon[i].lat, polygon[i].long);
+  //     var marker = new L.Marker(markerLocation);
+  //     this.map.addLayer(marker);
+  //   }
+  // }
 
 }
