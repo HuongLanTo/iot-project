f2a4501fc982aa53ba79c8871e1128d3d19a8665
Huong Lan To <tohuonglan1705@gmail.com>
2020-07-29 17:31:30 +0700

    update

--- web_client/src/app/pages/new-user/new-user.component.ts
+++ web_client/src/app/pages/new-user/new-user.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Role, role } from 'src/app/models/role';
 import { UserService } from 'src/app/services/user.service';
 import { ToastrService } from 'ngx-toastr';
 import { stringify } from 'querystring';
