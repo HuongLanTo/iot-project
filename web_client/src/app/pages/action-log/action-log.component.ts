e8fe39642cfca04835f5105b4144afc8ca0b1b00
Huong Lan To <tohuonglan1705@gmail.com>
2020-08-03 02:39:46 +0700

    add filter for action log page

--- web_client/src/app/pages/action-log/action-log.component.ts
+++ web_client/src/app/pages/action-log/action-log.component.ts
@@ -11,12 +11,23 @@ import { stringify } from 'querystring';
 })
 export class ActionLogComponent implements OnInit {
   actionLogList: any = [];
-  filter = {};
-  currentActionLog: any = {};
+  filter = {
+    action_user: undefined,
+    action_type: undefined,
+    collection_store: undefined,
+    action_time: undefined
+  };
+  currentActionLog: any = [];
   private currentPage: number = 1;
   public totalItems: number;
   private sizePage = 5;
   temp: any;
+
+  // search
+  searchActionUser: any = "";
+  searchActionType: any = "";
+  searchCollectionType: any = "";
+  searchActionTime: any = ""
 
   constructor(
     private actionLogService: ActionlogService,
@@ -42,6 +53,8 @@ export class ActionLogComponent implements OnInit {
     await this.actionLogService.getActionLogList(filter, currentPage, sizePage).then((data: any) => {
       this.totalItems = data.total;
       this.actionLogList = data.data;
+    }).catch(err => {
+      this.currentPage = 1;
     });
     this.spinnerService.hide();
   }
@@ -60,6 +73,41 @@ export class ActionLogComponent implements OnInit {
         return data[name]
       }
     }
+  }
+
+  async search() {
+    if (this.searchActionType.length == 0) {
+      this.filter.action_type = undefined;
+    } else {
+      this.filter.action_type = this.searchActionType;
+    }
+    if (this.searchActionUser.length == 0) {
+      this.filter.action_user = undefined;
+    } else {
+      this.filter.action_user = this.searchActionUser;
+    }
+    if (this.searchCollectionType.length == 0) {
+      this.filter.collection_store = undefined;
+    } else {
+      this.filter.collection_store = this.searchCollectionType;
+    }
+    if (this.searchActionTime.length == 0) {
+      this.filter.action_time = undefined;
+    } else {
+      this.filter.action_time = this.searchActionTime;
+    }
+    this.currentPage = 1;
+    await this.getActionLogList(this.filter, this.currentPage, this.sizePage);
+  }
+
+  async reset() {
+    this.filter.action_time = undefined;
+    this.filter.action_user = undefined;
+    this.filter.action_type = undefined;
+    this.filter.collection_store = undefined;
+    this.searchActionTime = this.searchActionType = this.searchActionUser = this.searchCollectionType = "";
+    this.currentPage = 1;
+    await this.getActionLogList(this.filter, this.currentPage, this.sizePage);
   }
 }
 
