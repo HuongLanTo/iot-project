b0aced90dac8198a301e628d21d532e5f4a51d12
Huong Lan To <tohuonglan1705@gmail.com>
2020-08-04 13:03:20 +0700

    update login

--- web_client/src/app/pages/login/login.component.ts
+++ web_client/src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy, OnChanges } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -8,11 +9,12 @@ import { AuthService } from '../../services/auth.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  public username: string;
-  public password: string;
+  public username: string = "";
+  public password: string = "";
 
   constructor(
     public authService: AuthService,
+    private toastrService: ToastrService,
     public router: Router
   ) {}
 
@@ -29,11 +31,22 @@ export class LoginComponent implements OnInit {
       username: this.username,
       password: this.password
     }
-    await this.authService.signIn(userLogin);
-    if (!await this.authService.isApprovedAndActive()) {
-      this.authService.logout();
+    if (this.username.length == 0) {
+      this.toastrService.warning("Tên đăng nhập không được để trống.");
     }
-    
+    if (this.password.length == 0) {
+      this.toastrService.warning("Mật khẩu không được để trống.");
+    }
+    if (this.username.length != 0 && this.username.length != 0) {
+      await this.authService.signIn(userLogin)
+      .then(data => {})
+      .catch(err => {
+        this.toastrService.warning("Tên đăng nhập hoặc mật khẩu không chính xác.")
+      });
+      if (!await this.authService.isApprovedAndActive()) {
+        this.authService.logout();
+      }
+    }
   }
 
 }
